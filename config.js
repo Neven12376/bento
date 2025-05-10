3b72751521e46c7a9690c23c8f970ebe9c947461
Neven12376 <153312960+Neven12376@users.noreply.github.com>
2025-05-10 12:20:41 -0500

    Update config.js
    12 hour format

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'Neven',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
