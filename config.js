bbf1248832b89691c17e06d114669b21731b4786
Neven12376 <153312960+Neven12376@users.noreply.github.com>
2025-05-10 12:23:02 -0500

    Update config.js
    google

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'Google',
+			icon: 'search',
+			link: 'https://google.com',
 		},
 		{
 			id: '6',
